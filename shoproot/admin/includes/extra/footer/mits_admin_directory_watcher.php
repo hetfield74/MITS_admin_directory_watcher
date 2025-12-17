72c091feb4d7d52e47e7585b3a57ed25da020936
hetfield74 <hetfield@merz-it-service.de>
2025-12-17 22:48:25 +0100

    some changes

--- shoproot/admin/includes/extra/footer/mits_admin_directory_watcher.php
+++ shoproot/admin/includes/extra/footer/mits_admin_directory_watcher.php
@@ -12,6 +12,8 @@
  * --------------------------------------------------------------
  */
 
+defined( '_VALID_XTC' ) or die( 'Direct Access to this location is not allowed.' );
+
 if (defined('DIR_ADMIN') && DIR_ADMIN !== 'admin') {
     $realAdminFolderName = trim(DIR_ADMIN, '/');
     $realAdminDir = DIR_FS_DOCUMENT_ROOT . $realAdminFolderName;
